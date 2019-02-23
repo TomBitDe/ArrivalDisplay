23e22a8bc2978156f50a599ff9bd8b521737fb6a
TomBitDe <tbit455674@gmail.com>
2019-02-23 12:01:34 +0100

    Better documentation.

--- src/main/java/com/home/config/package-info.java
+++ src/main/java/com/home/config/package-info.java
@@ -1,4 +1,20 @@
 /**
- * Arrival display configuration classes.
+ * Arrival display configuration classes.<br>
+ * <br>
+ * Configuration is done using arrivaldisplay.properties file. The following is an example:<br>
+ * <br>
+ * <code>
+ * arpo=DXB<br>
+ * startDate=2011-10-02<br>
+ * startTime=18:48:00<br>
+ * maxEntries=80<br>
+ * baseUri=http://localhost:8080/FlightService-war/rest/FlightService/arrivals<br>
+ * pollInterval=30<br>
+ * arpoUri=http://localhost:8080/AirportService-war/rest/AirportService<br>
+ * </code>
+ * <br>
+ * Date format is YYYY-MM-DD<br>
+ * Time format is HH:MM:SS<br>
+ * <br>
  */
 package com.home.config;
---
+++ src/main/java/com/home/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Arrival display application.<br>
+ * <br>
+ * The arrival display application uses REST web services for displaying flight arrivals:<br>
+ * <br>
+ * - FlightService/arrivals service delivers the arrival flights for an airport<br>
+ * - AirportService service retrieves the airport full name for the arrivals origin airport<br>
+ * <br>
+ */
+package com.home;
